f6220e7121f28355b6d5702ab4036c407d06362e
Gerigon <Rutger-100@hotmail.com>
2015-01-19 13:58:57 +0100

    Particles

--- source/ParticleWeapon.hx
+++ source/ParticleWeapon.hx
@@ -10,7 +10,7 @@ class ParticleWeapon extends Weapon
 {
 	
 	public var emitter:FlxEmitter;
-	private var particles:Int = 100;
+	private var particles:Int = 500;
 	
 	public function new(player:Player, FiringRate:Float, ProjectileSpeed:Int, WeaponImage:String, BulletImage:String, Damage:Int) 
 	{
@@ -21,20 +21,20 @@ class ParticleWeapon extends Weapon
 		for (i in 0...particles)
 		{
 			var particle:FlxParticle = new FlxParticle();
-			particle.makeGraphic(4, 4, 0xffe30b21);
+			particle.makeGraphic(4, 4, 0xfffffff);
 			particle.exists = false;
 			emitter.add(particle);
 		}
 		Reg.currentState.add(emitter);
 		
-		emitter.start(false,0.5,0.05);
+		emitter.start(false,0.5,0.003,0,1);
 		emitter.on = false;
 	}
 	
 	private override function shoot()
 	{
-		emitter.setYSpeed((Math.sin(owner.inputmanager.rightAngle * Math.PI / 180) * 100) -50, (Math.sin(owner.inputmanager.rightAngle * Math.PI / 180) * 100)+50);
-		emitter.setXSpeed((Math.cos(owner.inputmanager.rightAngle * Math.PI / 180) * 100)-50, (Math.cos(owner.inputmanager.rightAngle * Math.PI / 180) * 100)+50);
+		emitter.setYSpeed((Math.sin(owner.inputmanager.rightAngle * Math.PI / 180) * 100) -25, (Math.sin(owner.inputmanager.rightAngle * Math.PI / 180) * 100)+25);
+		emitter.setXSpeed((Math.cos(owner.inputmanager.rightAngle * Math.PI / 180) * 100)-25, (Math.cos(owner.inputmanager.rightAngle * Math.PI / 180) * 100)+25);
 		if (emitter.on == false)
 		{
 			emitter.on = true;

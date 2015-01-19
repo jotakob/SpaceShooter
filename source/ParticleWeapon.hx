dd8284d71fee53491406896ad4f7696e4b9f332a
HawkHunt <cobraluuk@hotmail.com>
2015-01-19 15:01:09 +0100

    modified Particle Emitter

--- source/ParticleWeapon.hx
+++ source/ParticleWeapon.hx
@@ -10,7 +10,7 @@ class ParticleWeapon extends Weapon
 {
 	
 	public var emitter:FlxEmitter;
-	private var particles:Int = 500;
+	private var particles:Int = 1000;
 	
 	public function new(player:Player, FiringRate:Float, ProjectileSpeed:Int, WeaponImage:String, BulletImage:String, Damage:Int) 
 	{
@@ -21,20 +21,20 @@ class ParticleWeapon extends Weapon
 		for (i in 0...particles)
 		{
 			var particle:FlxParticle = new FlxParticle();
-			particle.makeGraphic(4, 4, 0xfffffff);
+			particle.makeGraphic(4, 4, 0xffffb300);
 			particle.exists = false;
 			emitter.add(particle);
 		}
 		Reg.currentState.add(emitter);
 		
-		emitter.start(false,0.5,0.003,0,1);
+		emitter.start(false,0.5,0.003,0,2);
 		emitter.on = false;
 	}
 	
 	private override function shoot()
 	{
 		emitter.setYSpeed((Math.sin(owner.inputmanager.rightAngle * Math.PI / 180) * 100) -25, (Math.sin(owner.inputmanager.rightAngle * Math.PI / 180) * 100)+25);
-		emitter.setXSpeed((Math.cos(owner.inputmanager.rightAngle * Math.PI / 180) * 100)-25, (Math.cos(owner.inputmanager.rightAngle * Math.PI / 180) * 100)+25);
+		emitter.setXSpeed((Math.cos(owner.inputmanager.rightAngle * Math.PI / 180) * 100) -25, (Math.cos(owner.inputmanager.rightAngle * Math.PI / 180) * 100)+25);
 		if (emitter.on == false)
 		{
 			emitter.on = true;

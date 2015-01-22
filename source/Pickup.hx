d7601642a67b44f8d3d908397402e7ac99d5f398
HawkHunt <cobraluuk@hotmail.com>
2015-01-22 14:53:55 +0100

    added Pickup Class

---
+++ source/Pickup.hx
@@ -0,0 +1,18 @@
+package ;
+import flixel.effects.particles.FlxEmitter;
+import flixel.effects.particles.FlxParticle;
+
+/**
+ * ...
+ * @author Luuk
+ */
+class Pickup extends GameObject
+{
+
+	public function new(X:Float=0, Y:Float=0, Width:Float=0, Height:Float=0, Level:BaseLevel) 
+	{
+		super(X, Y, Width, Height, Level);
+		
+	}
+	
+}

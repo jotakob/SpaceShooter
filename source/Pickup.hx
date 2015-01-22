c734d1576aa071b9c3f030753fada44f54d0cef3
HawkHunt <cobraluuk@hotmail.com>
2015-01-22 15:15:18 +0100

    Pickups have a particle emitter

--- source/Pickup.hx
+++ source/Pickup.hx
@@ -1,18 +1,29 @@
 package ;
 import flixel.effects.particles.FlxEmitter;
 import flixel.effects.particles.FlxParticle;
-
+import flixel.util.FlxColor;
 /**
  * ...
  * @author Luuk
  */
-class Pickup extends GameObject
+class Pickup extends GameObject 
 {
-
+    public var emitter:FlxEmitter;
+	private var particles:Int = 2;
+	
 	public function new(X:Float=0, Y:Float=0, Width:Float=0, Height:Float=0, Level:BaseLevel) 
 	{
 		super(X, Y, Width, Height, Level);
 		
 	}
 	
+	for ( i in 0...particles)
+	{
+		private var particle: FlxParticle = new FlxParticle();
+		particle.MakeGraphic(2, 2, 0xffff0000);
+		particle.exist = false;
+		emitter.add(particle);
+	}
+	add(emitter);
+	emitter.start();
 }

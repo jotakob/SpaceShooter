13abf2b7959f287717e680df9ee79ae8b1ed91cb
HawkHunt <cobraluuk@hotmail.com>
2015-01-23 12:14:27 +0100

    Pickup sort off complete

--- source/Pickup.hx
+++ source/Pickup.hx
@@ -5,6 +5,7 @@ import flixel.FlxSprite;
 import flixel.util.FlxColor;
 import flixel.util.FlxPoint;
 import flixel.util.FlxVelocity;
+import flixel.util.FlxRandom;
 
 /**
  * ...
@@ -15,19 +16,25 @@ class Pickup extends GameObject
     public var emitter:FlxEmitter;
 	private var particles:Int = 500;
 	private var pickupImage = new FlxSprite();
+	private var randomColor:Int;
+	private var particleSize:Int;
+	
 	
 	public function new(X:Float=0, Y:Float=0, Width:Float=25, Height:Float=25, Level:BaseLevel) 
 	{
 		super(X, Y, Width, Height, Level);
+		
 		emitter = new FlxEmitter(1700, 800);
 		pickupImage.loadGraphic(AssetPaths.engineer_pickup_card__png, false, 32, 32, false);
 		pickupImage.x = 1700- pickupImage.width/2;
-		pickupImage.y = 800 - pickupImage.width/2;
+		pickupImage.y = 800 - pickupImage.width / 2;
+			
 		for ( i in 0...particles)
 		{
 			var particle: FlxParticle = new FlxParticle();
-			particle.loadGraphic(AssetPaths.pickup_particle_glow__png, true, 5, 5, false);
-			
+			particleSize = Std.random(3) + 3;
+			randomColor = FlxRandom.colorExt(0,255,0,255,0,255,255,255);
+			particle.makeGraphic(particleSize, particleSize, randomColor, false);
 			particle.exists = false;
 			emitter.add(particle);
 			particle.maxVelocity.x = 20;
@@ -35,13 +42,13 @@ class Pickup extends GameObject
 			emitter.setXSpeed( -2000, 2000);
 			emitter.setYSpeed( -2000, 2000);
 			
+			
 		}
 		Reg.currentState.add(emitter);
 		emitter.start(false, 0.2, 0, 0, 0);
 		Reg.currentState.add(pickupImage);
+		}
 		
 		
-		
-	}
 	
 }

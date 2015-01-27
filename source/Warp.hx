46a161cbbf0a3098f4f093b92113923c5a6443ec
Jakob M <github@jakob.mendelsohn.de>
2015-01-27 12:57:18 +0100

    new level

---
+++ source/Warp.hx
@@ -0,0 +1,16 @@
+package ;
+
+/**
+ * ...
+ * @author JJM
+ */
+class Warp extends GameObject
+{
+
+	public function new(X:Float=0, Y:Float=0, Width:Float=0, Height:Float=0, Level:BaseLevel) 
+	{
+		super(X, Y, Width, Height, Level);
+		
+	}
+	
+}
